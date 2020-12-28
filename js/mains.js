138dfcaac5a382130d314bc8c3fa06338abad5af
Mohammed <el-med7@outlook.com>
2020-12-28 15:38:12 +0100

    exo4

---
+++ js/mains.js
@@ -0,0 +1,18 @@
+let nom = 'dio'
+
+let prenom = 'brando'
+
+let ville = 'chicago'
+prompt(ville)
+
+console.log(prenom, nom);
+alert(ville)
+
+// exo2 
+
+let nomUn = 'giorno'
+let preNon = 'giovana'
+let villeDepart = 'bruxelles'
+let destination = 'Tokyo'
+
+console.log('Le voyage de' + ' ' + villeDepart + ' ' + 'vers' +' ' + destination + ' ' + 'a été enregistré au nom de'+ ' ' + nomUn + ' ' + preNon);
